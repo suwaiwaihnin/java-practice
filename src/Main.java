ddd4431bafa2dd2e0a11de2dbcb6565634e37d4e
unknown <suwaiwaihnin20@gmail.com>
2022-07-17 21:23:37 +0630

    問題2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.time.LocalDate;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        List<User> users = List.of(
+                new User("佐藤美咲", LocalDate.of(1990, 1, 1)),
+                new User("鈴木太郎", LocalDate.of(1991, 2, 2)),
+                new User("山田一郎", LocalDate.of(2003, 3, 3)),
+                new User("鈴木花子", LocalDate.of(2002, 4, 4)));
     }
 }
