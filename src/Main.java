317823008c0c4dbbead91c738e3fbef1790079b3
unknown <suwaiwaihnin20@gmail.com>
2022-07-18 00:15:01 +0630

    Userクラスを作成し、usersを表示する処理を記載

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,7 @@ public class Main {
                 new User("鈴木太郎", LocalDate.of(1991, 2, 2)),
                 new User("山田一郎", LocalDate.of(2003, 3, 3)),
                 new User("鈴木花子", LocalDate.of(2002, 4, 4)));
+        System.out.println("【すべてのユーザーを表示する】");
+        users.forEach(u -> System.out.printf("名前: %s, 生年月日: %s\n", u.getName(), u.getBirthdate()));
     }
 }
